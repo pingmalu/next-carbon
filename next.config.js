51705f4f6e768b01e90b7c9ae78f06ce9ac4be5b
malu <malu@malu.me>
2019-07-02 17:26:45 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 const withOffline = require('next-offline')
 
+// const isProd = process.env.NODE_ENV === 'production'
+// module.exports = {
+//   // You may only need to add assetPrefix in the production.
+//   assetPrefix: isProd ? '//nuxt3.me/dist' : ''
+// }
+
 module.exports = withOffline({ dontAutoRegisterSw: true })
